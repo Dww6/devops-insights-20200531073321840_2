17e15ced58021d8aa3d68e0f104e965f0c9b3cae
Daniel Wilson <danintendone@gmail.com>
2020-06-04 21:57:33 +0000

    Ajusted to replace spaces with "+" for calling weather API request

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -5,8 +5,9 @@ function Zip(props) {
     const [validationError, setValidationError] = useState(null);
 
     const validate = (event) => {
-        const cityNamePattern = /^[a-zA-Z\s]+$/;
-        const valid = cityNamePattern.test(event.target.value);
+    	const spacesReplaced = event.target.value.split(' ').join('+');
+        const cityNamePattern = /^[a-zA-Z\+]+$/;
+        const valid = cityNamePattern.test(spacesReplaced);
         if (!valid) {
             setValidationError('City Name must be at least 1 character and can consist only of Upper and Lower case letters and Spaces');
             props.clearResponse();
